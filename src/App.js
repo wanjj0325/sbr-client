bbbc64262932ecb6d68f7d0488c337081890d2b0
wanjj0325 <wanjunjie3@hotmail.com>
2025-01-23 14:16:39 +0800

    update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Home from './Home';
+function App() {
+  return (
+    <div className="App">
+      <h2>Welcome to our front end</h2>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/student/StudentView.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const StudentView = () => {
+  return (
+    <section>
+      <table>
+        <thead>
+            <tr>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
+        </thead>
+      </table>
+    </section>
+  )
+}
+
+export default StudentView
